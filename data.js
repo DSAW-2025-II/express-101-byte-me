8717be1e2c20637977ed6a9eb2d116c811060b04
Sofy <sofymurillo400@gmail.com>
2025-09-19 14:40:25 -0500

    Agregar archivos nuevos

---
+++ data.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    id: "0000336152",
+    name: "Sofy Alejandra",
+    lastname: "Prada Murillo",
+    email: "sofyprmu@unisabana.edu.co"
+  },
+  {
+    id: "0000328378",
+    name: "Esteban",
+    lastname: "Sequeda Henao",
+    email: "estebansehe@unisabana.edu.co"
+  }
+];
+
+module.exports = users;
